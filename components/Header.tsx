6d83f06be58fbfcbeda47a65a7fb8de96381fd5c
Maritherecua <mrtrsarroyo@yahoo.com>
2026-09-06 14:25:33 -0700

    centering header title and updating font size.

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="border-b border-zinc-200 dark:border-zinc-800">
-            <div id="header-title" className="text-lg font-semibold">Maria T Arroyo</div>
+            <div id="header-title" className="pt-4 text-center text-2xl font-semibold">Maria T Arroyo</div>
             <nav className="mx-auto flex max-w-3xl items-center justify-between px-16 py-4">
                 <Link href="/" className="text-lg font-semibold">
                     My Portfolio
