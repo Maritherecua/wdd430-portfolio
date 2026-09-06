6fd39358581124b508188468a3610a5fd9f58268
Maritherecua <mrtrsarroyo@yahoo.com>
2026-09-06 12:57:13 -0700

    components: Added ProjectCard and ProjectList with TypeScript interfaces

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMo
-        < Header />
-        { children }
-        < Footer />
-        e} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
